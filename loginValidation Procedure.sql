b18497b587049495ed566e8c7bbeeb22f95540d4
Maximilian Schulten <maxunlimited1222@gmail.com>
2024-11-21 14:04:20 -0500

    Add files via upload

---
+++ loginValidation Procedure.sql
@@ -0,0 +1,26 @@
+-- Validating a users login request
+CREATE PROCEDURE validateLogin
+    @username VARCHAR(50),
+    @password CHAR(60)
+AS
+BEGIN
+    -- Declare variables to hold the result
+    DECLARE @playerID INT;
+    DECLARE @resultMessage NVARCHAR(100);
+
+    -- Check if the username and password match
+    SELECT @playerID = playerID
+    FROM player
+    WHERE username = @username AND password = @password;
+
+    IF @playerID IS NOT NULL
+    BEGIN
+        -- Successful login
+        SELECT @playerID AS playerID, 'Login successful' AS resultMessage;
+    END
+    ELSE
+    BEGIN
+        -- Login failed
+        SELECT NULL AS playerID, 'Invalid username or password' AS resultMessage;
+    END
+END;
